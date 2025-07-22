34788cadb7328c470905266a8d0705a4c34be828
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2025-07-21 20:35:03 -0500

    ✨ feat: integra FormKit para formularios dinámicos
    - Se añadió la dependencia `@formkit/vue` al proyecto.
    - Se actualizó `NewProductView.vue` para incluir un formulario básico utilizando componentes de FormKit.
    - Se actualizó `package-lock.json` para reflejar los cambios en las dependencias.

--- src/views/admin/NewProductView.vue
+++ src/views/admin/NewProductView.vue
@@ -7,6 +7,12 @@ import Link from "@/components/Link.vue";
   <div>
     <Link to="products">Volver</Link>
     <h1 class="text-4xl font-black my-10">Nuevo Producto</h1>
+    <div class="flex justify-center bg-white shadow">
+
+    </div>
+    <FormKit type="form">
+      <FormKit type="text" label="Nombre" name="name" />
+    </FormKit>
   </div>
 </template>
 
